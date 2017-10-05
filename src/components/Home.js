7e8602b0ab976b87c5282dc0289c2e9c4de22a85
gavin771 <gavin.samuels10@gmail.com>
2017-10-04 22:36:32 -0500

    Updated components to load artiste data

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,34 @@
 import React, { Component } from 'react'
 
+import Banner from './Banner'
+import ArtisteList from './ArtisteList'
+
+const API = 'http://localhost:3004/artistes'
+
 class Home extends Component {
-    render () {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            artistes: []
+        }
+    }
+
+    componentDidMount() {
+        fetch(API, {
+            method: 'GET'
+        })
+            .then(response => response.json())
+            .then(json => {
+                this.setState({ artistes: json })
+            })
+    }
+
+    render() {
         return (
             <div>
-                Home Page
+                <Banner />
+                <ArtisteList data={this.state.artistes} />
             </div>
         )
     }
